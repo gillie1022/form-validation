94af5983778cf6ae48df5de87096497fecbdae61
gillie1022 <dave.gillie@icloud.com>
2020-04-30 17:30:32 -0400

    created validateName

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Add validation!');
+console.log("Add validation!");
+
+let form = document.querySelector("#parking-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  validateName()
+  
+});
+
+function validateName() {
+  let nameInput = document.querySelector("#name");
+  let nameValue = nameInput.value;
+  let nameField = nameInput.parentElement;
+
+  if (nameValue !== "") {
+    nameField.classList.remove("input-invalid");
+    nameField.classList.add("input-valid");
+  } else {
+    nameField.classList.remove("input-valid");
+    nameField.classList.add("input-invalid");
+  }
+}

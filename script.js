599a3dd5e284c855d1f4b6936006f6dae3a9f429
gillie1022 <dave.gillie@icloud.com>
2020-05-01 14:42:58 -0400

    three kinda works

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 console.log("Add validation!");
 
 let form = document.querySelector("#parking-form");
+let nameInput = document.querySelector("#name");
+let nameField = nameInput.parentElement;
+document.getElementsByTagName("label")[0].setAttribute("id", "name-label");
+let nameLabel = document.querySelector("#name-label");
+let carYear = document.querySelector("#car-year");
+carYear.setAttribute("type", "number");
+let carMake = document.querySelector("#car-make");
+let carModel = document.querySelector("#car-model");
+let carField = document.querySelector("#car-field");
+document.getElementsByTagName("label")[1].setAttribute("id", "car-label");
+let carLabel = document.querySelector("#car-label");
+let startDate = document.querySelector("#start-date");
+let startDateField = startDate.parentElement;
+document
+  .getElementsByTagName("label")[2]
+  .setAttribute("id", "start-date-label");
+let startDateLabel = document.querySelector("#start-date-label");
+let days = document.querySelector("#days");
+let daysField = days.parentElement;
+document.getElementsByTagName("label")[3].setAttribute("id", "days-label");
+let daysLabel = document.querySelector("#days-label");
+let creditCard = document.querySelector("#credit-card");
+let creditCardField = creditCard.parentElement;
+document
+  .getElementsByTagName("label")[4]
+  .setAttribute("id", "credit-card-label");
+let creditCardLabel = document.querySelector("#credit-card-label");
+let cvvInput = document.querySelector("#cvv");
+let cvvField = cvvInput.parentElement;
+document.getElementsByTagName("label")[5].setAttribute("id", "cvv-label");
+let cvvLabel = document.querySelector("#cvv-label");
+let expirationInput = document.querySelector("#expiration");
+let expirationField = expirationInput.parentElement;
+document
+  .getElementsByTagName("label")[6]
+  .setAttribute("id", "expiration-label");
+let expirationLabel = document.querySelector("#expiration-label");
 
 function validateName() {
-  let nameInput = document.querySelector("#name");
   let nameValue = nameInput.value;
-  let nameField = nameInput.parentElement;
-  document.getElementsByTagName("label")[0].setAttribute("id", "name-label");
-  let nameLabel = document.querySelector("#name-label");
   if (nameValue !== "") {
     nameField.classList.remove("input-invalid");
     nameField.classList.add("input-valid");
@@ -19,20 +52,22 @@ function validateName() {
   }
 }
 function validateCar() {
-  let carYear = document.querySelector("#car-year");
   let carYearValue = carYear.value;
-  let carMake = document.querySelector("#car-make");
   let carMakeValue = carMake.value;
-  let carModel = document.querySelector("#car-model");
   let carModelValue = carModel.value;
-  let carField = document.querySelector("#car-field");
-  document.getElementsByTagName("label")[1].setAttribute("id", "car-label");
-  let carLabel = document.querySelector("#car-label");
-
-  if (carYearValue !== "" && carMakeValue !== "" && carModelValue !== "") {
+  if (
+    carYearValue > 1900 &&
+    carYearValue < 2021 &&
+    carMakeValue !== "" &&
+    carModelValue !== ""
+  ) {
     carField.classList.remove("input-invalid");
     carField.classList.add("input-valid");
     carLabel.textContent = "Car";
+  } else if (carYearValue < 1900) {
+    carField.classList.remove("input-valid");
+    carField.classList.add("input-invalid");
+    carLabel.textContent = "Year must be a between 1900 and 2020!";
   } else {
     carField.classList.remove("input-valid");
     carField.classList.add("input-invalid");
@@ -40,13 +75,7 @@ function validateCar() {
   }
 }
 function validateStartDate() {
-  let startDate = document.querySelector("#start-date");
   let startDateValue = startDate.value;
-  let startDateField = startDate.parentElement;
-  document
-    .getElementsByTagName("label")[2]
-    .setAttribute("id", "start-date-label");
-  let startDateLabel = document.querySelector("#start-date-label");
   if (startDateValue !== "") {
     startDateField.classList.remove("input-invalid");
     startDateField.classList.add("input-valid");
@@ -58,12 +87,7 @@ function validateStartDate() {
   }
 }
 function validateDays() {
-  let days = document.querySelector("#days");
   let daysValue = days.value;
-  let daysField = days.parentElement;
-  document.getElementsByTagName("label")[3].setAttribute("id", "days-label");
-  let daysLabel = document.querySelector("#days-label");
-
   if (daysValue !== "") {
     daysField.classList.remove("input-invalid");
     daysField.classList.add("input-valid");
@@ -75,13 +99,7 @@ function validateDays() {
   }
 }
 function validateCreditCard() {
-  let creditCard = document.querySelector("#credit-card");
   let creditCardValue = creditCard.value;
-  let creditCardField = creditCard.parentElement;
-  document
-    .getElementsByTagName("label")[4]
-    .setAttribute("id", "credit-card-label");
-  let creditCardLabel = document.querySelector("#credit-card-label");
   if (creditCardValue !== "") {
     creditCardField.classList.remove("input-invalid");
     creditCardField.classList.add("input-valid");
@@ -93,11 +111,7 @@ function validateCreditCard() {
   }
 }
 function validateCvv() {
-  let cvvInput = document.querySelector("#cvv");
   let cvvValue = cvvInput.value;
-  let cvvField = cvvInput.parentElement;
-  document.getElementsByTagName("label")[5].setAttribute("id", "cvv-label");
-  let cvvLabel = document.querySelector("#cvv-label");
   if (cvvValue !== "") {
     cvvField.classList.remove("input-invalid");
     cvvField.classList.add("input-valid");
@@ -109,13 +123,7 @@ function validateCvv() {
   }
 }
 function validateExpiration() {
-  let expirationInput = document.querySelector("#expiration");
   let expirationValue = expirationInput.value;
-  let expirationField = expirationInput.parentElement;
-  document
-    .getElementsByTagName("label")[6]
-    .setAttribute("id", "expiration-label");
-  let expirationLabel = document.querySelector("#expiration-label");
   if (expirationValue !== "") {
     expirationField.classList.remove("input-invalid");
     expirationField.classList.add("input-valid");
@@ -137,12 +145,3 @@ form.addEventListener("submit", function (event) {
   validateCvv();
   validateExpiration();
 });
-
-// create seperate functions for car year
-
-  // create validateCarYear
-    // type must be a number
-    // number must be > 1900
-    // number must be < current year + 1
-
-
